3fc0c09bd361d68d8bd6347ac754c174bf9327fb
Dion Dirza <dion.dirza@veritrans.co.id>
2017-02-26 00:17:00 +0700

    Remove http protocol

--- src/common/repository.js
+++ src/common/repository.js
@@ -1,5 +1,5 @@
 const getAllPokemon = (offset = 0) =>
-  fetch(`http://pokeapi.salestock.net:8000/api/v2/pokemon/?offset=${offset*30}&limit=30`)
+  fetch(`//pokeapi.salestock.net:8000/api/v2/pokemon/?offset=${offset*30}&limit=30`)
     .then((res) => {
       if (res.ok) return res.json();
       return null;
@@ -7,7 +7,7 @@ const getAllPokemon = (offset = 0) =>
     .catch(err => console.error(err));
 
 const getPokemonDetail = (id) =>
-  fetch(`http://pokeapi.salestock.net:8000/api/v2/pokemon/${id}`)
+  fetch(`//pokeapi.salestock.net:8000/api/v2/pokemon/${id}`)
     .then((res) => {
       if (res.ok) return res.json();
       return null;
